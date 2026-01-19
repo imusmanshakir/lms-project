71784613e235a30fb0401fcf556c15ec97f18641
Usman Malik <imusmanshakir@gmail.com>
2026-01-19 14:48:10 +0500

    cors

--- lms-backend/server.js
+++ lms-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "http://localhost:3000",
+    "http://localhost:5173",
     "https://usman-lms.vercel.app"
   ],
   credentials: true
